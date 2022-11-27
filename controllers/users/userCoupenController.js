07d903f43226691ee75b286f9f5fd45aa2f5cafb
Anurag MK <anuragmk10@gmail.com>
2022-11-27 13:16:52 +0530

    cart total minimum amount commit

--- controllers/users/userCoupenController.js
+++ controllers/users/userCoupenController.js
@@ -6,19 +6,22 @@ const userCoupenModel = require("../../models/userHelper/userCoupenModel");
 const applyCoupen = async (req, res) => {
   let userData = req.session.user;
   let coupenCode = req.body.coupenCode;
-  if(coupenCode){
-    let totalAmount = await userCartHelper.getTotalAmount(userData._id);
-    let TOTAL = totalAmount;
+  let totalAmount = await userCartHelper.getTotalAmount(userData._id);
+  let TOTAL = totalAmount;
+  if (coupenCode && TOTAL >= 5000) {
     let coupenDetails = await userCoupenModel.getCoupenDetails(coupenCode);
-    await userCoupenModel.getDiscount(coupenDetails, TOTAL).then((response) => {
     
-  
-      res.json(response);
-    });
-  }else{
-    res.json(response)
+      await userCoupenModel
+        .getDiscount(coupenDetails, TOTAL)
+        .then((response) => {
+          
+          res.json(response);
+        });
+    
+  } else {
+    console.log("response3", response);
+    res.json(response);
   }
-
 };
 
 module.exports = {
